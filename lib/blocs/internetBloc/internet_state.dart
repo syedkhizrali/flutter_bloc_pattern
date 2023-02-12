32a037a4bc702490a624feef4a99acd8f6a964f5
syedkhizrali <syedkhizr321@gmail.com>
2023-02-12 19:58:22 +0530

    initial commit

---
+++ lib/blocs/internetBloc/internet_state.dart
@@ -0,0 +1,15 @@
+abstract class InternetState{
+
+}
+
+class InternetInitialState extends InternetState{
+
+}
+
+class InternetLostState extends InternetState{
+
+}
+
+class InternetGainedState extends InternetState{
+
+}
---
+++ lib/homepage/HomePage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_pattern/blocs/internetBloc/internet_bloc.dart';
+import 'package:flutter_bloc_pattern/blocs/internetBloc/internet_state.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: BlocConsumer<InternetBloc,InternetState>(
+          listener: (context, state){
+            if(state is InternetGainedState){
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Internet Connected"), backgroundColor: Colors.green,));
+            }else if(state is InternetLostState){
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Internet Disconnected"), backgroundColor: Colors.green,));
+            }
+          },
+          builder: (context, state){
+            if(state is InternetGainedState){
+              return const Text("Connected");
+            }else if(state is InternetLostState){
+              return const Text("Not Connected");
+            }else {
+              return const Text("Loading...");
+            }
+
+          },
+        ),
+      )), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
